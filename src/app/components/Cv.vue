76486c3ae15e17600d1e4ce47857ea5825db33e5
Danilo Hernandez <dhernandez0032@gmail.com>
2019-10-01 19:51:52 -0600

    update 01/10/2019 19:51

--- src/app/components/Cv.vue
+++ src/app/components/Cv.vue
@@ -1,88 +1,84 @@
 <template>
-	<div>
-		<!--div class="row">
-			<div class="col-md-12 text-center p-4 bg-primary text-white">
-				Desarrollador Web
+	<div class="row vh-100">
+		<div class="col-md-12 portada text-center text-white pt-5" style="font-size: 2rem">
+			Desarrollador Web
+		</div>
+		<div class="col-md-4 bg-dark">
+			<div class="text-center my-4">
+				<img height="100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZIIvMebaX69XxiSod3KwAyxOQvGpv0-FDJB8kFsDhK7TXrJm2Rw" alt="img" class="rounded-circle">
 			</div>
-		</div-->
-		<div class="row vh-100">
-			<div class="col-md-4 bg-dark">
-				<div class="text-center my-4">
-					<img height="100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZIIvMebaX69XxiSod3KwAyxOQvGpv0-FDJB8kFsDhK7TXrJm2Rw" alt="img" class="rounded-circle">
-				</div>
-				<p class="h5 text-white">Información personal</p>
-				<div class="table-responsive">
-					<table class="table text-warning">
-						<tbody>
-							<tr>
-								<th scope="row"><i class="fas fa-user mr-2"></i>Nombre</th>
-								<td>Danilo Hernandez</td>
-							</tr>
-							<tr>
-								<th scope="row"><i class="fas fa-map mr-2"></i>Nacionalidad</th>
-								<td>guatemalteco</td>
-							</tr>
-							<tr>
-								<th scope="row"><i class="fas fa-language mr-2"></i>Lenguaje</th>
-								<td>Español</td>
-							</tr>
-							<p class="h5 text-white mt-3">Contacto</p>
-							<tr>
-								<th scope="row"><i class="fas fa-mobile mr-2"></i>Celular</th>
-								<td>5581-3691</td>
-							</tr>
-							<tr>
-								<th scope="row"><i class="fas fa-envelope mr-2"></i>Correo</th>
-								<td>edh@gmail.com</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<p class="h5 text-white border-bottom pb-2">Enlaces</p>
-				<div class="btn-group mt-2">
-					<a class="btn btn-outline-warning" href="http://github.com/dhernandz7" target="_blank">
-						<i class="fab fa-github mr-2"></i>
-						Github
-					</a>
-					<a class="btn btn-outline-warning" href="http://twitter.com/dhernandz7" target="_blank">
-						<i class="fab fa-twitter mr-2"></i>
-						Twitter
-					</a>
-					<a class="btn btn-outline-warning" href="http://twitter.com/dhernandz7" target="_blank">
-						<i class="fab fa-facebook mr-2"></i>
-						Facebook
-					</a>
-				</div>
+			<p class="h5 text-white">Información personal</p>
+			<div class="table-responsive">
+				<table class="table text-warning">
+					<tbody>
+						<tr>
+							<th scope="row"><i class="fas fa-user mr-2"></i>Nombre</th>
+							<td>Danilo Hernandez</td>
+						</tr>
+						<tr>
+							<th scope="row"><i class="fas fa-map mr-2"></i>Nacionalidad</th>
+							<td>guatemalteco</td>
+						</tr>
+						<tr>
+							<th scope="row"><i class="fas fa-language mr-2"></i>Lenguaje</th>
+							<td>Español</td>
+						</tr>
+						<p class="h5 text-white mt-3">Contacto</p>
+						<tr>
+							<th scope="row"><i class="fas fa-mobile mr-2"></i>Celular</th>
+							<td>5581-3691</td>
+						</tr>
+						<tr>
+							<th scope="row"><i class="fas fa-envelope mr-2"></i>Correo</th>
+							<td>edh@gmail.com</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
-			<div class="col-md-8 bg-white pt-3">
-				<p class="h5 border-bottom p-2">Acerca de mí</p>
-				<p class="text-justify">
-					Desarrollador web...
-				</p>
-				<p class="h5 border-bottom p-2">Educación</p>
-				<ul class="list-group my-3">
-					<li class="list-group-item">
-						<span class="font-weight-bold mr-2">2018</span>
-						Séptimo semestre de Ingeniería en Sistemas
-					</li>
-				</ul>
-				<p class="h5 border-bottom p-2">Conocimiento</p>
-				<div class="row my-3">
-					<div class="col-md-6">
-						<label for=""><i class="fab fa-laravel mr-2"></i>Laravel</label>
-						<div class="progress">
-							<div class="progress-bar w-75 text-center" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">75%</div>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for=""><i class="fas fa-database mr-2"></i>Base de datos</label>
-						<div class="progress">
-							<div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">75%</div>
-						</div>
+			<p class="h5 text-white border-bottom pb-2">Enlaces</p>
+			<div class="btn-group mt-2">
+				<a class="btn btn-outline-warning" href="http://github.com/dhernandz7" target="_blank">
+					<i class="fab fa-github mr-2"></i>
+					Github
+				</a>
+				<a class="btn btn-outline-warning" href="http://twitter.com/dhernandz7" target="_blank">
+					<i class="fab fa-twitter mr-2"></i>
+					Twitter
+				</a>
+				<a class="btn btn-outline-warning" href="http://twitter.com/dhernandz7" target="_blank">
+					<i class="fab fa-facebook mr-2"></i>
+					Facebook
+				</a>
+			</div>
+		</div>
+		<div class="col-md-8 bg-white pt-3">
+			<p class="h5 border-bottom p-2">Acerca de mí</p>
+			<p class="text-justify">
+				Desarrollador web...
+			</p>
+			<p class="h5 border-bottom p-2">Educación</p>
+			<ul class="list-group my-3">
+				<li class="list-group-item">
+					<!--span class="font-weight-bold mr-2">2018</span-->
+					Ingeniería en Sistemas
+				</li>
+			</ul>
+			<p class="h5 border-bottom p-2">Conocimiento</p>
+			<div class="row my-3">
+				<div class="col-md-6">
+					<label for=""><i class="fab fa-laravel mr-2"></i>Laravel</label>
+					<div class="progress">
+						<div class="progress-bar w-75 text-center" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">75%</div>
 					</div>
 				</div>
-				<p class="h5 border-bottom p-2">Hobbies</p>
+				<div class="col-md-6">
+					<label for=""><i class="fas fa-database mr-2"></i>Base de datos</label>
+					<div class="progress">
+						<div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">75%</div>
+					</div>
+				</div>
 			</div>
+			<p class="h5 border-bottom p-2">Hobbies</p>
 		</div>
 	</div>
 </template>
